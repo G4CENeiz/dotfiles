fd9ce2bd8ce5a616060d1f068a7f39ef8d849a4b
Baihaqi <116696555+G4CENeiz@users.noreply.github.com>
2026-07-17 23:35:42 +0700

    fix: treesitter config for nvim-treesitter v1+

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -41,27 +41,25 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "c", "html", "javascript", "json", "lua",
-          "markdown", "nix", "python", "rust", "toml", "tsx",
-          "typescript", "vim", "vimdoc", "yaml",
+    opts = {
+      ensure_installed = {
+        "bash", "c", "html", "javascript", "json", "lua",
+        "markdown", "nix", "python", "rust", "toml", "tsx",
+        "typescript", "vim", "vimdoc", "yaml",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
         },
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<BS>",
-          },
-        },
-      })
-    end,
-  },
+      },
+    },
+  };
 
   -- File explorer
   {
